401819e54dc5e79209f10ff5b8b3b2d5c2145339
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-25 22:13:59 +0100

    Change entities advisesTo type notation

--- src/squire/type.ts
+++ src/squire/type.ts
@@ -1,7 +1,8 @@
 import { Character } from "../character/type";
+import { Fighter } from "../fighter/type";
 
 export type Squire = Character & {
-  servesTo: string;
+  servesTo: Fighter;
   ballism: number;
   phrase: "I'm a loser";
 };
