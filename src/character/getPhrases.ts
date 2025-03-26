b7ce8ff9a929e8a4bb0e332135276e83161c0733
Ivet Lopez <ivet19@live.com>
2025-03-27 00:21:01 +0100

    Remove unnecessary guard clause

--- src/character/getPhrases.ts
+++ src/character/getPhrases.ts
@@ -6,9 +6,6 @@ import { Squire } from "../squire/types";
 export const getPhrases = (
   characters: (King | Fighter | Adviser | Squire)[],
 ): string[] => {
-  if (characters.length === 0) {
-    throw new Error("Cannot get phrases from an empty array.");
-  }
 
   return characters.map((character) => character.phrase);
 };
