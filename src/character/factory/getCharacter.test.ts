a8158cd6ff3100049d5b21779dcabca2b7ccb863
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-27 01:31:50 +0100

    Add Character entity factory function

---
+++ src/character/factory/getCharacter.test.ts
@@ -0,0 +1,57 @@
+import getCharacter from "./getCharacter";
+import killCharacter from "../killCharacter";
+import makeCharacterSpeak from "../makeCharacterSpeak";
+import { Actions, Portrait } from "../types";
+
+describe("Given the getCharacter function", () => {
+  describe("When it receives 'LeBron', 'James', 40, and an empty portrait", () => {
+    const name = "LeBron";
+    const lastName = "James";
+    const age = 40;
+    const portrait: Portrait = {
+      url: "",
+      description: "",
+    };
+
+    test("Then it should return a character with name 'LeBron'", () => {
+      const expectedName = "LeBron";
+
+      const character = getCharacter(name, lastName, age, portrait);
+
+      expect(character.name).toBe(expectedName);
+    });
+
+    test("Then it should return a character with last name 'James'", () => {
+      const expectedLastName = "James";
+
+      const character = getCharacter(name, lastName, age, portrait);
+
+      expect(character.lastName).toBe(expectedLastName);
+    });
+
+    test("Then it should return a 40 year old character", () => {
+      const expectedAge = 40;
+
+      const character = getCharacter(name, lastName, age, portrait);
+
+      expect(character.age).toBe(expectedAge);
+    });
+
+    test("Then it should return a 40 year old character that is alive", () => {
+      const character = getCharacter(name, lastName, age, portrait);
+
+      expect(character.isAlive).toBe(true);
+    });
+
+    test("Then it should return a character with actions kill and speak", () => {
+      const expectedActions: Actions = {
+        kill: killCharacter,
+        speak: makeCharacterSpeak,
+      };
+
+      const character = getCharacter(name, lastName, age, portrait);
+
+      expect(character.actions).toStrictEqual(expectedActions);
+    });
+  });
+});
