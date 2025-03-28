cfae53ee33db229e1d77ee63d751a0a82676d33a
Ivet Lopez <ivet19@live.com>
2025-03-28 20:03:23 +0100

    Add character's age test to chacacter's card tests

--- src/components/CharacterCard/getCharacterCard.test.ts
+++ src/components/CharacterCard/getCharacterCard.test.ts
@@ -3,7 +3,7 @@ import getCharacterCard from "./getCharacterCard.js";
 
 describe("Given a CharacterCard componen", () => {
   describe("When it recives a 'María Sarmiento'", () => {
-    test("Then it should show a character's name inside a heading", () => {
+    test("Then it should show 'María Sarmiento' inside a heading", () => {
       const screen = document.createElement("div");
       const expectedCharacterName = "María Sarmiento";
 
@@ -14,6 +14,19 @@ describe("Given a CharacterCard componen", () => {
 
       expect(characterName).not.toBeNull();
       expect(characterName?.textContent).toBe(expectedCharacterName);
+    });
+
+    test("Then it should show 'Age: 50 years", () => {
+      const screen = document.createElement("div");
+      const expectedCharacterAge = "Age: " + 50 + " years";
+
+      const CharacterCard = getCharacterCard(mariaSarmiento);
+      screen.appendChild(CharacterCard);
+
+      const characterAge = screen.querySelector("span");
+
+      expect(characterAge).not.toBeNull();
+      expect(characterAge?.textContent).toBe(expectedCharacterAge);
     });
   });
 });
