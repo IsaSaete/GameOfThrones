e8a2a7da92fe478cf3c2186f828bbd5ab2084409
Isabel Sáenz <isasaete@gmail.comn>
2025-03-29 17:50:13 +0100

    Add function for character emoji and function test

---
+++ src/character/components/CharacterCard/__tests__/getCharacterEmoji.test.ts
@@ -0,0 +1,19 @@
+import { bradPitt } from "../../../../squire/fixtures";
+import getCharacterEmoji from "../getCharacterEmoji";
+
+describe("Given the getCharacterEmoji function", () => {
+  describe("When it receives a Bradd Pitt", () => {
+    test("Then it should show a shield emoji", () => {
+      const screen = document.createElement("div");
+      const expectedEmoji = "🛡️";
+
+      const character = bradPitt;
+      const characterEmoji = getCharacterEmoji(character);
+
+      screen.appendChild(characterEmoji);
+
+      expect(characterEmoji).not.toBeNull();
+      expect(characterEmoji?.textContent).toBe(expectedEmoji);
+    });
+  });
+});
--- src/character/components/CharacterCard/getCharacterCard.ts
+++ src/character/components/CharacterCard/getCharacterCard.ts
@@ -1,24 +1,26 @@
 import { Character } from "../../types.js";
+import getCharacterIcon from "./getCharacterEmoji.js";
 import getCharacterPortrait from "./getCharacterPortrait.js";
 
-const getCharacterCard = ({
-  name,
-  lastName,
-  age,
-  portrait,
-}: Character): HTMLElement => {
+const getCharacterCard = (character: Character): HTMLElement => {
   const CharacterCard = document.createElement("article");
   CharacterCard.className = "card-character";
 
-  CharacterCard.innerHTML = `
-  <div class="character__info">
-  <h2 class="character__name">${name} ${lastName}</h2>
-  <span class="character__age">Age: ${age} years</span>
-  </div>
+  const containtInfo = document.createElement("div");
+  containtInfo.className = "character__info";
+
+  containtInfo.innerHTML = `
+  <h2 class="character__name">${character.name} ${character.lastName}</h2>
+  <span class="character__age">Age: ${character.age} years</span>
   `;
 
-  const characterPortrait = getCharacterPortrait(portrait);
+  CharacterCard.appendChild(containtInfo);
+
+  const characterPortrait = getCharacterPortrait(character.portrait);
   CharacterCard.prepend(characterPortrait);
+
+  const characterEmoji = getCharacterIcon(character);
+  containtInfo.appendChild(characterEmoji);
 
   return CharacterCard;
 };
